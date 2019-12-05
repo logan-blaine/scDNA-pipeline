1de562100b2324970abbecf907916c2ffea30629
Pellman Lab <pellmanlab@784f435deebf.dfci.harvard.edu>
2019-12-05 18:20:20 -0500

    fixed align script, cleaned up repo

--- align.sh
+++ align.sh
@@ -2,9 +2,13 @@
 #SBATCH --mail-user=LoganJ_Blaine@dfci.harvard.edu --mail-type=ALL
 #SBATCH -c 2 --mem 1G
 
+CORES=${SLURM_CPUS_PER_TASK:-1}
+SAMPLES="samples.tsv"
+
 SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output}"
 
-snakemake -prk --rerun-incomplete  \
-    -j 50 --local-cores $SLURM_CPUS_PER_TASK --latency-wait 60 \
-    --configfile config.yaml --cluster-config cluster.yaml \
+snakemake -prk --rerun-incomplete --nolock \
+    -j 80 --local-cores $CORES --latency-wait 60 \
+    --configfile config.yaml --config samples=$SAMPLES \
+	--cluster-config cluster.yaml \
     --cluster "sbatch $SBATCH_OPTS" align
--- test_snakemake.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-snakemake -prk --rerun-incomplete -n --configfile config.yaml

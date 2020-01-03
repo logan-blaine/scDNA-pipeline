499c14cecf1a22bd7f979a07c4bc5ba0f36a3d66
Pellman Lab <pellmanlab@784f435deebf.dfci.harvard.edu>
2020-01-02 19:34:30 -0500

    added normal samples (for svaba) to config

--- snakemake.sh
+++ snakemake.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=LoganJ_Blaine@dfci.harvard.edu --mail-type=ALL
 #SBATCH -c 2 --mem 1G
 
-SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output} -x node07"
+SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output}"
 
 snakemake --keep-going --restart-times 2 --rerun-incomplete \
     -j 50 --local-cores ${SLURM_CPUS_PER_TASK:-1} --latency-wait 60 \

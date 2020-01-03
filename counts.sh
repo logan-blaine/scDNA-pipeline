3a8dde224caf1a8ea6c8036dadb33fe69856295c
Logan <loganb@kraken2.cm.cluster>
2020-01-03 12:07:49 -0500

    changes to launch scripts

--- counts.sh
+++ counts.sh
@@ -2,9 +2,10 @@
 #SBATCH --mail-user=LoganJ_Blaine@dfci.harvard.edu --mail-type=ALL
 #SBATCH -c 2 --mem 1G
 
-SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output} -x node07"
+SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output}"
 
-snakemake count --keep-going --rerun-incomplete \
-    -j 50 --local-cores ${SLURM_CPUS_PER_TASK:-1} --latency-wait 60 \
-    --configfile config.yaml --cluster-config cluster.yaml \
-    --cluster "sbatch $SBATCH_OPTS"
+snakemake --keep-going --restart-times 2 --rerun-incomplete \
+    -j 10 --local-cores ${SLURM_CPUS_PER_TASK:-1} --latency-wait 60 \
+    --configfile config.yaml \
+    --cluster-config cluster.yaml \
+    --cluster "sbatch $SBATCH_OPTS" counts

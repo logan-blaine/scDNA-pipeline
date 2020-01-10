1b1d82fdd85614dbc2265d90cf3442a9f3ebba6b
Pellman Lab <pellmanlab@784f435deebf.dfci.harvard.edu>
2020-01-10 10:52:45 -0500

    fam4 files for sv testing

---
+++ svaba.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mail-user=LoganJ_Blaine@dfci.harvard.edu --mail-type=ALL
+#SBATCH -c 2 --mem 1G
+
+SBATCH_OPTS="--parsable -c {cluster.cpus} --mem {cluster.mem_gb}G --output {cluster.output}"
+
+snakemake --keep-going --restart-times 2 --rerun-incomplete \
+    -j 10 --local-cores ${SLURM_CPUS_PER_TASK:-1} --latency-wait 60 \
+    --configfile config.yaml \
+    --cluster-config cluster.yaml \
+    --cluster "sbatch $SBATCH_OPTS" --config samples=fam4.tsv

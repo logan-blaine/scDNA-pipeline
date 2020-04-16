06fd1d608576bd7649b54a15982328b9927e20ba
Pellman Lab <pellmanlab@784f435deebf.local>
2020-04-16 15:09:27 -0400

    bypassing pysam to write the vcf ouptut

--- scripts/filter_vcf_by_tbl.py
+++ scripts/filter_vcf_by_tbl.py
@@ -9,14 +9,14 @@ output_vcf = snakemake.output[0]
 min_count = 3
 # win_size = snakemake.params['window_size']
 
-print("Reading "+vcf_path)
+# print("Reading "+vcf_path)
 pc = PairedVcf(vcf_path)
 pairs = pc.get_pairs()
 
 chr_pos_str=['chr1', 'pos1', 'str1', 'chr2', 'pos2', 'str2']
 id_cols = chr_pos_str+['sample']
 
-print("Reading "+table_path)
+# print("Reading "+table_path)
 tbl = pd.read_csv(table_path)
 tbl_deduped = tbl.groupby(id_cols).first().reset_index()
 tbl_deduped['n']=tbl_deduped.groupby(chr_pos_str)['sample'].transform('count')
@@ -25,13 +25,14 @@ tbl_filtered=tbl_deduped.query(f'n==1 and count>={min_count} and hq_count>0')
 vcf_head=VariantFile(vcf_path).header
 vcf_head.add_line(f'##command=recount_svs.py {vcf_path} {table_path} > {output_vcf}')
 
-print("Writing "+output_vcf)
-vcf_out=VariantFile(output_vcf, mode='w', header=vcf_head)
-for _, row in tbl_filtered.iterrows():
-    hash = '\t'.join(map(str, row[chr_pos_str]))
-    if hash in pairs:
-        # print(row['sample'])
-        mate1, mate2 = pairs[hash].get_pair()
-        vcf_out.write(mate1.rec)
-        vcf_out.write(mate2.rec)
-vcf_out.close()
+# print("Writing "+output_vcf)
+# vcf_out=VariantFile(output_vcf, mode='w', header=vcf_head)
+
+with open(output_vcf, 'w') as f:
+    f.write(str(vcf_head))
+    for _, row in tbl_filtered.iterrows():
+        hash = '\t'.join(map(str, row[chr_pos_str]))
+        if hash in pairs:
+            mate1, mate2 = pairs[hash].get_pair()
+            f.write(str(mate1.rec))
+            f.write(str(mate2.rec))

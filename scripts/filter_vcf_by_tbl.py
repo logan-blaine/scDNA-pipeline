64fe1044f01fba1bdbd76593e9c7294441d2bb7b
Pellman Lab <pellmanlab@784f435deebf.local>
2020-04-16 14:21:02 -0400

    some prints to find the source of a confusing segfault

--- scripts/filter_vcf_by_tbl.py
+++ scripts/filter_vcf_by_tbl.py
@@ -9,12 +9,14 @@ output_vcf = snakemake.output[0]
 min_count = 3
 # win_size = snakemake.params['window_size']
 
+print("Reading "+vcf_path)
 pc = PairedVcf(vcf_path)
 pairs = pc.get_pairs()
 
 chr_pos_str=['chr1', 'pos1', 'str1', 'chr2', 'pos2', 'str2']
 id_cols = chr_pos_str+['sample']
 
+print("Reading "+table_path)
 tbl = pd.read_csv(table_path)
 tbl_deduped = tbl.groupby(id_cols).first().reset_index()
 tbl_deduped['n']=tbl_deduped.groupby(chr_pos_str)['sample'].transform('count')
@@ -22,8 +24,9 @@ tbl_filtered=tbl_deduped.query(f'n==1 and count>={min_count} and hq_count>0')
 
 vcf_head=VariantFile(vcf_path).header
 vcf_head.add_line(f'##command=recount_svs.py {vcf_path} {table_path} > {output_vcf}')
-vcf_out=VariantFile(output_vcf, mode='w', header=vcf_head)
 
+print("Writing "+output_vcf)
+vcf_out=VariantFile(output_vcf, mode='w', header=vcf_head)
 for _, row in tbl_filtered.iterrows():
     hash = '\t'.join(map(str, row[chr_pos_str]))
     if hash in pairs:
